2e20e8215a72054266e8f81e15017beaf342ab62
MOABdev <abmohammed659@gmail.com>
2024-09-14 14:04:13 +0000

    Initial configs

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+<h1>Hello World welcome to the disney youtube clone </h1>
+    </div>
+  );
+}
+
+export default App;
