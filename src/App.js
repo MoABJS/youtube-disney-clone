711da37752443041ec1da30ee9a2750143c77d2e
MOABdev <abmohammed659@gmail.com>
2024-09-21 22:10:26 +0000

    Done with details page, Clone complete

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Login from "./components/Login";
 import Navbar from "./components/Navbar";
 import Home from "./Home"
+import Details from "./components/Details";
 
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Login />} />
           <Route path="/home" element={<Home />} />
+          <Route path="/details/:id" element={<Details />} />
         </Routes>
     </Router>
   );
