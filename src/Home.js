25792118afc467c6607832ac2819809ff3cc2efd
MOABdev <abmohammed659@gmail.com>
2024-09-21 12:01:40 +0000

    Done with the other views

--- src/Home.js
+++ src/Home.js
@@ -1,12 +1,74 @@
 import styled from "styled-components";
 import ImageSlider from "./components/ImageSlider";
 import Viewers from "./components/Viewers";
+import Recommends from "./components/Recommends";
+import NewDisney from "./components/NewDisney";
+import Originals from "./components/Originals";
+import Trending from "./components/Trending";
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { setMovies } from "./config/movie/movieSlice";
+import { db } from "./config/firebase";
+import { collection, getDocs } from "firebase/firestore";
+import { selectUserName } from "./config/userSlice";
 
 const Home = () => {
+  const dispatch = useDispatch();
+  const username = useSelector(selectUserName);
+
+  useEffect(() => {
+    let recommends = [];
+    let newDisney = [];
+    let originals = [];
+    let trending = [];
+
+    const getMovies = async () => {
+      try {
+        const queryMovies = await getDocs(collection(db, "movies"));
+        queryMovies.docs.forEach((doc) => {
+          switch (doc.data().type) {
+            case "recommend":
+              recommends = [...recommends, { id: doc.id, ...doc.data() }];
+              break;
+            case "new":
+              newDisney = [...newDisney, { id: doc.id, ...doc.data() }];
+              break;
+            case "original":
+              originals = [...originals, { id: doc.id, ...doc.data() }];
+              break;
+            case "trending":
+              trending = [...trending, { id: doc.id, ...doc.data() }];
+              break;
+          }
+        });
+        // console.log(recommends);
+
+        dispatch(
+          setMovies({
+            recommend: recommends,
+            newDisney: newDisney,
+            original: originals,
+            trending: trending,
+          })
+        );
+      } catch (err) {
+        console.error(err);
+      }
+    };
+
+    getMovies();
+  }, [username]);
+
+
+
   return (
     <Container>
       <ImageSlider />
       <Viewers />
+      <Recommends />
+      <NewDisney />
+      <Originals />
+      <Trending />
     </Container>
   );
 };
--- src/config/store.js
+++ src/config/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit"
 import userReducer from "../config/userSlice"
+import movieReducer from "../config/movie/movieSlice"
 
 export default configureStore({
     reducer: {
         user: userReducer,
+        movie: movieReducer,
     },
     middleware: (getDefaultMiddleware) =>
          getDefaultMiddleware({
